18fdcf78fec6bacc92ae391f9c053122de32a424
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-07-29 22:04:44 -0500

    Working on loading screens

---
+++ Zombies vs Plants/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    [SerializeField] private int startScreenIndex;
+    private int currentSceneIndex;
+    [SerializeField] private string gameScreenName;
+    [SerializeField] private string splashScreenName;
+    [SerializeField] private int secondsToWaitAndLoad;
+
+
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    
+
+    private IEnumerator WaitForTime()
+    {
+            yield return new WaitForSeconds(secondsToWaitAndLoad);
+            LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        if (currentSceneIndex + 1 == 1) StartCoroutine(WaitForTime());
+        else SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
